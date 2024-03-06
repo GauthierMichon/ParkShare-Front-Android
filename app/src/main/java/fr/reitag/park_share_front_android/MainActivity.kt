7605dd2ee4b86bfdcfbe7f15142745d6949c00f4
GMICHON <gauthier.michon@ynov.com>
2024-03-06 09:21:42 +0100

    init

---
+++ app/src/main/java/fr/reitag/park_share_front_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.reitag.park_share_front_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
